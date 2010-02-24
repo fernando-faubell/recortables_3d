03698bbe6bf787ac1624c2facfd6ab258081bd9e
Fernando Faubell Valls <fernando@garbera.WAG160N>
2010-02-24 19:36:55 +0100

    Pruebas con el farbtastic.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+
+$(document).ready(function(){
+
+  $('#colorpicker_recortable_color_fondo').farbtastic('#recortable_color_fondo');
+  $('#colorpicker_recortable_color_aristas').farbtastic('#recortable_color_aristas');
+
+});
